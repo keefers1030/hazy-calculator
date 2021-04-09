f65502ffe54c10638ab4f64791789e88f4dece39
Keith March Mistler <kmistler@gmail.com>
2021-04-08 21:52:19 -0400

    changed operand values within the switch case i[1]

--- index.js
+++ index.js
@@ -16,78 +16,57 @@ Our upstream data is inconsistent, so we _intentionally_ accept it and deal with
 */
 
 
-//function 1
+
+
+const filterString = (arr) => {
     //loop through array
-    //remove empty inputs
+    for (let i = 0; i < arr.length; i++) {
+        let element = arr[i];
+        if (elment[i] === elment[i + 1]) {
+            block.push(element[i])
+        }
+
+    }
     //removes/ignores non numeric numbers
-
-
     //change string numbers to integers
     //change null to 0
+    {
+        const inputs = ['+', '-', '*', '/', 'null', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+        let validInputs = arr.filter(invalid => {
+            !inputs.includes(invalid)
+        })
+
+        return validInputs
+    }
+}
+
 
 
 //function 2
 //Make calculations
+const calculationSteps = (arr) => {
+    const operand = ['+', '-', '*', '/']
 
-
-
-const filterString = (arr) => {
-    const inputs = ['+', '-', '*', '/', 'null', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-    for (let i = 0; i < arr.length; i++) {
-        let element = arr[i]
-
-        {
-            let validInputs = arr.filter(invalid => {
-                !inputs.includes(invalid)
-            })
-
-            return validInputs
-        }
-    }
-}
-
-
-
-console.log(filterString([8, 9, 0, '9', '8', null]))
-
-
-const checkString = (arr) => {
-
-    //loop through array
-    for (let i = 0; i < arr.length; i++) {
-        const element = arr[i]; {
-
-            const operand = [`+`, `-`, `*`, `/`]
-            //if operand, go to calculate
-            if (arr[i] === operand) {
-                calculate(num1, num2)
-            }
-            else if (arr[i] === '')
-        }
-    }
-}
-
-const calculate = (arr) => {
     switch (operand) {
         case '+':
-            return (num1 + num2);
+            return (i[0] + i[2]);
 
         case '-':
-            return (num1 - num2);
+            return (i[0] - i[2]);
 
         case '*':
-            return (num1 * num2);
+            return (i[0] * i[2]);
 
         case '/':
-            return (num1 / num2);
+            return (i[0] / i[2]);
         //if no operand, returns NaN
         default: NaN
             break;
     }
 
 }
-    
 
+module.exports(calculationSteps)
 
 
